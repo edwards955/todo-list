7fab6d8b8fdd6f35e77e3ef831cc76cc134b9f97
edwards955 <getignorantfriday@gmail.com>
2023-08-28 15:00:10 -0400

    Add method to add to Projects; edit styling

--- src/Project.js
+++ src/Project.js
@@ -1,6 +1,11 @@
 const Project = (title) => {
     const todos = [];
-    return { title, todos };
+
+    const addToProject = (todo) => {
+        todos.push(todos);
+    }
+
+    return { title, todos, addToProject };
 };
 
 export default Project;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import ToDo from './ToDo';
 import Project from './Project';
+import './style.css';
+
 const todo1 = ToDo('Task 1', 'This is my first task', Date.now(), 'low');
 const todo2 = ToDo('Task 2', 'This is my second task', Date.now(), 'medium');
 const todo3 = ToDo('Task 3', 'This is my third task', Date.now(), 'high');
 
 const testProject = Project('Things');
-testProject.todos.push(todo1);
-testProject.todos.push(todo2);
-testProject.todos.push(todo3);
+testProject.addToProject(todo1);
+testProject.addToProject(todo2);
+testProject.addToProject(todo3);
 
 console.log(testProject);
